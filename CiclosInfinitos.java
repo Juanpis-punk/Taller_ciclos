8bbb9fa177059995f72de887191dfb1a950a4c1a
Emmanuel Rios <emmanuelrios628@gmail.com>
2026-08-31 08:25:27 -0500

    archivo de emma 2

---
+++ CiclosInfinitos.java
@@ -0,0 +1,54 @@
+public class CiclosInfinitos {
+
+    public static void main(String[] args) {
+
+        // 1. Ciclo infinito utilizando while (true)
+
+        /*
+        while (true) {
+            System.out.println("Ciclo infinito con while");
+        }
+        */
+
+
+        // 2. Ciclo infinito utilizando for (;;)
+
+        /*
+        for (;;) {
+            System.out.println("Ciclo infinito con for");
+        }
+        */
+
+
+        // 3. Ciclo infinito utilizando do-while
+
+        /*
+        do {
+            System.out.println("Ciclo infinito con do-while");
+        } while (true);
+        */
+
+
+        // 4. Ciclo infinito con una condición que siempre es verdadera
+
+        /*
+        int numero = 10;
+
+        while (numero > 0) {
+            System.out.println("La condición siempre es verdadera");
+        }
+        */
+
+
+        // 5. Ciclo infinito utilizando una variable booleana
+
+        /*
+        boolean continuar = true;
+
+        while (continuar) {
+            System.out.println("Ciclo infinito con variable booleana");
+        }
+        */
+
+    }
+}
